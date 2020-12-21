308bfce20992da1b777a4c2e890a8f43d3f7eeed
taoqf <tao_qiufeng@126.com>
2020-12-21 08:27:41 +0800

    exec

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,16 @@
 const test = require('ava');
+const fs = require('fs/promises');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('run command in current dir', async (t) => {
+	const readme = await a('cat ./readme.md');
+	const file = await fs.readFile('./readme.md', 'utf-8');
+	t.is(readme, file.trimEnd());
+});
+
+test('run command in special dir', async (t) => {
+	const settings = await a('cat ./settings.json', './.vscode/');
+	const file = await fs.readFile('./.vscode/settings.json', 'utf-8');
+	t.is(settings, file);
 });
